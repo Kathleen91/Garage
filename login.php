0880fdcc94e72a144ebddeaf6d20ceb4f851718f
Kathleen91 <k.rakotoson@cfa-itis.com>
2021-09-22 15:25:24 +0200

    encore M

--- login.php
+++ login.php
@@ -2,5 +2,11 @@
 
   if(($_POST['mail'] == "mail@mail.fr") && ($_POST['mdp'] == "ggbg")) {
     die("test validé");
+
+
+    
+  } else {
+    echo "Le mail/mot de passe est erroné."<br>"Veuillez réessayer.";
   }
+
 ?>
